99aa4953c9fcbf3b162a6186a811db9fd2033e62
Pepe <natanjabie09@gmail.com>
2022-08-10 01:36:05 -0300

    New Commit Only Script

---
+++ analysis.py
@@ -0,0 +1,28 @@
+import pandas
+import matplotlib.pyplot as plt
+
+
+#df = pd.read_excel('F:\FilesProyectVSC\ProyectoINTI\Libro5.xlsx')
+#print(df)
+
+tabla = pandas.read_csv('registro_mipyme_09-05-2022.csv')
+
+#print(pd.options.display.max_rows) 
+print(tabla)
+
+
+"""
+print(tabla['Sector'].value_counts())
+print(tabla['Provincia'].value_counts())
+print(tabla['Sector'].value_counts(normalize=True))
+print(tabla['Provincia'].value_counts(normalize=True))
+"""
+#tabla['Sector'].value_counts().to_frame()
+"""
+tabla['Sector'].value_counts().plot(kind="bar")
+plt.show()
+
+
+tabla['Provincia'].value_counts().plot(kind="bar")
+plt.show()
+""" 
